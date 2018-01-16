6115a6d036018a897a0c7facbdea8d3beb0c3f17
Matheus Magalhaes <matheusr@cpqd.com.br>
2018-01-16 07:37:08 -0200

    Removes deprecated files from openWhisk PoC

--- set.js
+++
@@ -1,4 +0,0 @@
-function main(params) {
-  params.msg['_from_change'] = 'changenode';
-  return params.msg;
-}
--- switch.js
+++
@@ -1,4 +0,0 @@
-function main(params) {
-  params.msg['_from_switch'] = 'switchnode';
-  return params.msg;
-}
