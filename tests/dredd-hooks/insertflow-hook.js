dc3f052d22aeaacaa5399dc590d31104138b4ef5
Manuel Gavidia <manuelg@cpqd.com.br>
2018-04-04 11:03:16 -0300

    Adds dredd tests

---
+++ tests/dredd-hooks/insertflow-hook.js
@@ -0,0 +1,30 @@
+var hooks = require("hooks");
+var mongo = require('../../orchestrator/node_modules/mongodb');
+var config = require('../../orchestrator/config');
+
+hooks.beforeEach(function (transaction, done) {
+  hooks.log("before");
+  mongo.MongoClient.connect(config.mongodb.url, function(err, db) {
+    if (err) throw err;
+    hooks.log("Entering Database!");
+    var dbo = db.db("flowbroker_admin");
+    var myobj = {
+      "created": new Date('2014-01-22T14:56:59.301Z'),
+      "devices": [],
+      "enabled": true,
+      "heads": [],
+      "id": "aaaaaaaa",
+      "label": "zeroflow",
+      "red": [],
+      "templates": [],
+      "updated": new Date('2014-01-22T14:56:59.301Z')
+    };
+
+    dbo.collection("flows").insertOne(myobj, function(err, res) {
+      if (err) throw err;
+      hooks.log("1 flow inserted");
+      db.close();
+    });
+    done();
+  });
+});
