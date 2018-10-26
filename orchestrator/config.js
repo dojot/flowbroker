f3d2323bf1d7824d141a37233596a8a097075ccb
Rafael Augusto Scaraficci <raugusto@cpqd.com.br>
2018-10-26 10:08:48 -0300

    Changed the flowbroker to guarantee the events are processed in order
    Solution: Messages are moved from the Kafka 'device-data' subject
    to the queue 'event_queue' in Rabbitmq. Then they are pre-processed
    one at a time and tasks are pushed to queue 'task_queue' to be processed
    also one at a time. Once a task is processed if new tasks are spawned,
    they have higher priorities.

--- orchestrator/config.js
+++ orchestrator/config.js
@@ -21,14 +21,14 @@ module.exports = {
         }
     },
 
-
     'dataBroker': {
         url: process.env.DATA_BROKER_URL || "http://data-broker:80"
     },
 
     'amqp': {
         url: process.env.AMQP_URL || "amqp://rabbitmq",
-        queue: process.env.AMQP_QUEUE || "task_queue"
+        queue: process.env.AMQP_QUEUE || "task_queue",
+        event_queue: process.env.AMQP_EVENT_QUEUE || "event_queue"
     },
 
     'deploy': {
@@ -47,5 +47,43 @@ module.exports = {
         contextManagerAddress: process.env.CONTEXT_MANAGER_ADDRESS || "flowbroker-context-manager",
         contextManagerPort: process.env.CONTEXT_MANAGER_PORT || 5556,
         responseTimeout: process.env.CONTEXT_MANAGER_RESPONSE_TIMEOUT || 10000
-    }
+    },
+
+    'kafkaMessenger' : {
+        kafka: {
+            producer: {
+                "metadata.broker.list": process.env.KAFKA_HOSTS || "kafka:9092",
+                "compression.codec": "gzip",
+                    "retry.backoff.ms": 200,
+                    "message.send.max.retries": 10,
+                    "socket.keepalive.enable": true,
+                    "queue.buffering.max.messages": 100000,
+                    "queue.buffering.max.ms": 100,
+                    "batch.num.messages": 1000000,
+                    "dr_cb": true
+                },
+        
+                consumer: {
+                    "group.id": process.env.KAFKA_GROUP_ID || "flowbroker",
+                    "metadata.broker.list": process.env.KAFKA_HOSTS || "kafka:9092"
+                }
+            },
+            databroker: {
+              host: process.env.DATA_BROKER_URL || "http://data-broker",
+            },
+            auth: {
+              host: process.env.AUTH_URL || "http://auth:5000",
+            },
+            deviceManager: {
+              host: process.env.DEVICE_MANAGER_URL || "http://device-manager:5000",
+            },
+            dojot: {
+              managementService: process.env.DOJOT_SERVICE_MANAGEMENT || "dojot-management",
+              subjects: {
+                tenancy: process.env.DOJOT_SUBJECT_TENANCY || "dojot.tenancy",
+                devices: process.env.DOJOT_SUBJECT_DEVICES || "dojot.device-manager.device",
+                deviceData: process.env.DOJOT_SUBJECT_DEVICE_DATA || "device-data"
+              }
+            }
+        }
 };
