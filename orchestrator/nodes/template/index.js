90cdb0168f726b28e5175f62567d27d0215781fb
Raul Negreiros <rauln@cpqd.com.br>
2019-02-28 13:34:27 -0300

    Adds helper to stringify objects on template node

--- orchestrator/nodes/template/index.js
+++ orchestrator/nodes/template/index.js
@@ -8,6 +8,9 @@ const logger = require("../../logger").logger;
 class DataHandler extends dojot.DataHandlerBase {
     constructor() {
         super();
+        handlebars.registerHelper('stringify', function(context) {
+            return JSON.stringify(context);
+        });
     }
 
     /**
