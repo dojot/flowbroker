305edf76294ebdb749d27cf8062552cbda598439
Raul Negreiros <rauln@cpqd.com.br>
2019-05-16 10:39:40 -0300

    Adds the use of the logger in the executor.js

--- orchestrator/executor.js
+++ orchestrator/executor.js
@@ -3,10 +3,11 @@
 var amqp = require('./amqp');
 var config = require('./config');
 var nodes = require('./nodeManager').Manager;
+var logger = require("@dojot/dojot-module-logger").logger;
 
 module.exports = class Executor {
     constructor(contextHandler) {
-        console.log('[executor] initializing ...');
+        logger.debug('[executor] initializing ...');
         this.hop = this.hop.bind(this);
         this.producer = new amqp.AMQPProducer(config.amqp.queue, config.amqp.url, 2);
         this.consumer = new amqp.AMQPConsumer(config.amqp.queue, this.hop, config.amqp.url, 2);
@@ -26,19 +27,19 @@ module.exports = class Executor {
         try {
             event = JSON.parse(data);
         } catch (error) {
-            console.error("[amqp] Received event is not valid JSON. Ignoring");
+            logger.warn("[amqp] Received event is not valid JSON. Ignoring");
             return ack();
         }
 
         const at = event.flow.nodeMap[event.hop];
         // sanity check on received hop
         if (!at.hasOwnProperty('type')) {
-            console.error(`[executor] Node execution failed. Missing node type. Aborting flow ${event.flow.id}.`);
+            logger.warn(`[executor] Node execution failed. Missing node type. Aborting flow ${event.flow.id}.`);
             // TODO notify alarmManager
             return ack();
         }
 
-        console.log(`[executor] will handle node ${at.type}`);
+        logger.debug(`[executor] will handle node ${at.type}`);
         let handler = nodes.getNode(at.type, event.metadata.tenant);
         if (handler) {
             let metadata = {
@@ -57,7 +58,7 @@ module.exports = class Executor {
 
             Promise.race([handleMessagePromise, handleMessageTimeoutPromise])
                 .then((result) => {
-                    console.log(`[executor] hop (${at.type}) result: ${JSON.stringify(result)}`);
+                    logger.debug(`[executor] hop (${at.type}) result: ${JSON.stringify(result)}`);
                     for (let output = 0; output < at.wires.length; output++) {
                         let newEvent = result[output];
                         if (newEvent) {
@@ -77,12 +78,12 @@ module.exports = class Executor {
                     }
                     return ack();
                 }).catch( (error) => {
-                    console.warn(`[executor] Node (${at.type}) execution failed. Error: ${error}. Aborting flow ${event.flow.id}.`);
+                    logger.warn(`[executor] Node (${at.type}) execution failed. Error: ${error}. Aborting flow ${event.flow.id} branch execution.`);
                     // TODO notify alarmManager
                     return ack();
                 });
         } else {
-            console.error(`[executor] Unknown node ${at.type} detected. Igoring.`);
+            logger.warn(`[executor] Unknown node ${at.type} detected. Igoring. Aborting flow ${event.flow.id} branch execution.`);
             return ack();
         }
     }
