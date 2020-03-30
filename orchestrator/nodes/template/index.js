ca2bcde268e279dc42bb9c2fcbd0d3156198884b
Ernandes Azevedo Junior <ernandes_azevedo@stash.atlantico.com.br>
2020-03-30 16:46:15 -0300

    Added mathematical features in HandleBars

--- orchestrator/nodes/template/index.js
+++ orchestrator/nodes/template/index.js
@@ -12,6 +12,26 @@ class DataHandler extends dojot.DataHandlerBase {
         handlebars.registerHelper('stringify', function(context) {
             return JSON.stringify(context);
         });
+        
+        handlebars.registerHelper("math", function(lvalue, operator, rvalue, options) {
+            if (arguments.length < 4) {
+                // Operator omitted, assuming "+"
+                options = rvalue;
+                rvalue = operator;
+                operator = "+";
+            }
+                
+            lvalue = parseFloat(lvalue);
+            rvalue = parseFloat(rvalue);
+                
+            return {
+                "+": lvalue + rvalue,
+                "-": lvalue - rvalue,
+                "*": lvalue * rvalue,
+                "/": lvalue / rvalue,
+                "%": lvalue % rvalue
+            }[operator];
+        });        
     }
 
     /**
