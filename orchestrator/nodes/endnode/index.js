37e8ecdf8f01a79e6983939c2a0e8afed2f2abd1
marciofunes <marciofunes@gmail.com>
2021-04-29 08:57:50 -0300

    add json application map

--- orchestrator/nodes/endnode/index.js
+++ orchestrator/nodes/endnode/index.js
@@ -4,6 +4,8 @@ const dojot = require('@dojot/flow-node');
 const binaryParser = require('binary-parser')
 const splice = require('buffer-splice');
 const { Console } = require('console');
+const fs = require('fs');
+const aplication = require('./id-map.json');
 
 // Sample node implementation
 class DataHandler extends dojot.DataHandlerBase {
@@ -60,141 +62,48 @@ class DataHandler extends dojot.DataHandlerBase {
      */
     handleMessage(config, message) {
         try {
-            let FRMpayload = this._get(config.in, message);  
-            //let nwkSKey = Buffer.from(config.nsw, "hex");          
+            let FRMpayload = this._get(config.in, message);            
 
             // Module import
             var Parser = require("binary-parser").Parser;
 
-            //FRMpayload = "040002000F43C80F5C4124000044802D71412926E9412926E90024005500550055";
-
-            //var FRMpayload_raw = Buffer.from(FRMpayload, "hex");
-
-            var headStart = 0
-            var headEnd = 2    
-            var bodyEnd = FRMpayload.length
-            
-            var aplicacao = FRMpayload.substring(headStart, headEnd)
-            //timestamp_raw = FRMpayload.substring(timestampStart, bodyStart)
-            var body_raw = FRMpayload.substring(headEnd, bodyEnd)
-            
+            var headStart = 0;
+            var headEnd = 2;    
+            var bodyEnd = FRMpayload.length;
+                
+            var aplicacao = FRMpayload.substring(headStart, headEnd);
+            var body_raw = FRMpayload.substring(headEnd, bodyEnd);
+                
             var FRMpayload_buffer = Buffer.from(body_raw, "hex");
 
-            console.log("aplicação: " + aplicacao)    
-            console.log("body: " + body_raw)
+            console.log("aplicação: " + aplicacao); 
+            console.log("body: " + body_raw);
 
-            var aplicacao01 = new Parser()
-                .floatbe("Timestamp")
-                .floatbe("TensaoRMSFaseA")
-                .floatbe("TensaoRMSFaseB")
-                .floatbe("TensaoRMSFaseC")    
-                .floatbe("CorrenteRMSFaseA")
-                .floatbe("CorrenteRMSFaseB")
-                .floatbe("CorrenteRMSFaseC")
-                .floatbe("CorrenteRMSneutro")    
-                .floatbe("FrequenciaFaseA")
-                .floatbe("FrequenciaFaseB")
-                .floatbe("FrequenciaFaseC")    
-                .floatbe("PotenciaAtivaFaseA")
-                .floatbe("PotenciaAtivaFaseB")
-                .floatbe("PotenciaAtivaFaseC")
-                .floatbe("PotenciaAtivaTotal")    
-                .floatbe("PotenciaReativaFaseA")
-                .floatbe("PotenciaReativaFaseB")
-                .floatbe("PotenciaReativaFaseC")
-                .floatbe("PotenciaReativaTotal")    
-                .floatbe("PotenciaAparenteFaseA")
-                .floatbe("PotenciaAparenteFaseB")
-                .floatbe("PotenciaAparenteFaseC")
-                .floatbe("PotenciaAparenteTotal")    
-                .floatbe("FatordePotenciaFaseA")
-                .floatbe("FatordePotenciaFaseB")
-                .floatbe("FatordePotenciaFaseC")
-                .floatbe("FatordePotenciaTotal")    
-                .floatbe("ConsumoFaseA")
-                .floatbe("ConsumoFaseB")
-                .floatbe("ConsumoFaseC")
-                .floatbe("ConsumoTotal");
-                
-            var aplicacao04 = new Parser()
-                .floatbe("Timestamp")
-                .floatbe("TensaoCC")
-                .floatbe("CorrenteCC")
-                .floatbe("PotenciaCC")
-                .floatbe("EnergiaFornecida")
-                .floatbe("EnergiaConsumida")    
-                .uint16be("Temperatura")
-                .uint16be("Status")
-                .uint16be("SaudeBateria")
-                .uint16be("EstadoCarga");
-
-            var FRMpayload_decoded
-
-            switch(aplicacao){
-                case "01":
-                var FRMpayload_decoded = aplicacao01.parse(FRMpayload_buffer)
-                var json_endnode = {
-                    aplicacao: aplicacao,                                
-                    Timestamp: FRMpayload_decoded.Timestamp,
-                    TensaoRMSFaseA: FRMpayload_decoded.TensaoRMSFaseA,
-                    TensaoRMSFaseB: FRMpayload_decoded.TensaoRMSFaseB,                
-                    TensaoRMSFaseC: FRMpayload_decoded.TensaoRMSFaseC,
-                    CorrenteRMSFaseA: FRMpayload_decoded.CorrenteRMSFaseA,
-                    CorrenteRMSFaseB: FRMpayload_decoded.CorrenteRMSFaseB,
-                    CorrenteRMSFaseC: FRMpayload_decoded.CorrenteRMSFaseC,
-                    CorrenteRMSneutro: FRMpayload_decoded.CorrenteRMSneutro,
-                    FrequenciaFaseA: FRMpayload_decoded.FrequenciaFaseA,
-                    FrequenciaFaseB: FRMpayload_decoded.FrequenciaFaseB,
-                    FrequenciaFaseC: FRMpayload_decoded.FrequenciaFaseC,
-                    PotenciaAtivaFaseA: FRMpayload_decoded.PotenciaAtivaFaseA,
-                    PotenciaAtivaFaseB: FRMpayload_decoded.PotenciaAtivaFaseA,
-                    PotenciaAtivaFaseC: FRMpayload_decoded.PotenciaAtivaFaseC,
-                    PotenciaAtivaTotal: FRMpayload_decoded.PotenciaAtivaTotal,
-                    PotenciaReativaFaseA: FRMpayload_decoded.PotenciaReativaFaseA,
-                    PotenciaReativaFaseB: FRMpayload_decoded.PotenciaReativaFaseB,
-                    PotenciaReativaFaseC: FRMpayload_decoded.PotenciaReativaFaseC,
-                    PotenciaReativaTotal: FRMpayload_decoded.PotenciaReativaTotal,    
-                    PotenciaAparenteFaseA: FRMpayload_decoded.PotenciaReativaTotal,
-                    PotenciaAparenteFaseB: FRMpayload_decoded.PotenciaAparenteFaseB,
-                    PotenciaAparenteFaseC: FRMpayload_decoded.PotenciaAparenteFaseC,
-                    PotenciaAparenteTotal: FRMpayload_decoded.PotenciaAparenteTotal,     
-                    FatordePotenciaFaseA: FRMpayload_decoded.FatordePotenciaFaseA,
-                    FatordePotenciaFaseB: FRMpayload_decoded.FatordePotenciaFaseB,
-                    FatordePotenciaFaseC: FRMpayload_decoded.FatordePotenciaFaseC,
-                    FatordePotenciaTotal: FRMpayload_decoded.FatordePotenciaTotal,     
-                    ConsumoFaseA: FRMpayload_decoded.ConsumoFaseA,
-                    ConsumoFaseB: FRMpayload_decoded.ConsumoFaseB,
-                    ConsumoFaseC: FRMpayload_decoded.ConsumoFaseC,
-                    ConsumoTotal: FRMpayload_decoded.ConsumoTotal
-                };
-                this._set(config.out, json_endnode, message);
-                break;
-
-                case "04":
-                var FRMpayload_decoded = aplicacao04.parse(FRMpayload_buffer)      
-                var json_endnode = {
-                    aplicacao: aplicacao,                   
-                    TensaoCC: FRMpayload_decoded.TensaoCC,
-                    CorrenteCC: FRMpayload_decoded.CorrenteCC,
-                    PotenciaCC: FRMpayload_decoded.PotenciaCC,
-                    EnergiaFornecida: FRMpayload_decoded.EnergiaFornecida,
-                    EnergiaConsumida: FRMpayload_decoded.EnergiaConsumida,
-                    Temperatura: FRMpayload_decoded.Temperatura,
-                    Status: FRMpayload_decoded.Status,
-                    SaudeBateria: FRMpayload_decoded.SaudeBateria,
-                    EstadoCarga: FRMpayload_decoded.EstadoCarga
-                }
-                this._set(config.out, json_endnode, message);
-                break;
+            const reducer = (map, currentValue) => { 
+                const [key, value] = currentValue;
+                map.set(key, eval(value));
+                return map;
             }
 
-            console.log("FRMpayload_decoded: " + FRMpayload_decoded)
+            var myMap = Object.entries(aplication).reduce(reducer , new Map());
             
-            return Promise.resolve([message]);
+            if(myMap.get(aplicacao) == null){
+                
+                throw new Error("Aplicação não encontrada");
+
+            } else{
+                
+                var FRMpayload_decoded = myMap.get(aplicacao).parse(FRMpayload_buffer);
+                console.log(FRMpayload_decoded);
+                this._set(config.out, FRMpayload_decoded, message);
+                return Promise.resolve([message]); 
+            }          
 
         } //handle try (first) end 
         catch (error) {
+
             return Promise.reject(error);
+
         }
 
     }//handleMessage(config, message)
